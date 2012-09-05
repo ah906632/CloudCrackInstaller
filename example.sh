1735f2c23bb911074c513f14b378640219d3dbb1
Thomas Jetzinger <thomas@jetzinger.com>
2012-09-05 23:34:11 +0200

    Modified scripts

--- example.sh
+++ example.sh
@@ -3,6 +3,6 @@
 
 screen -D -RR
 
-#crunch 8 8 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\!\@\#\$\%\^\&\*\.\(\) | pyrit FILE.cap -e WLAN-SSID -i - attack_passthrough
-
 crunch 8 8 | pyrit -r FILE.cap -e WLAN-SSID -i - attack_passthrough
+#OR
+#crunch 8 8 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\!\@\#\$\%\^\&\*\.\(\) | pyrit FILE.cap -e WLAN-SSID -i - attack_passthrough
--- test.sh
+++ test.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 #lets check if everything is working fine
 
-pyrit
-cowpatty
-crunch 8 8 abc
-pyrit benchmark
+function checkInstallation {
+	if [ "$(which $1)" != "" ]
+	then 
+		echo "$1 installation: OK"
+	else
+		echo "$1 installation: FAILED"
+	fi
+}
+
+checkInstallation automake
+checkInstallation autoconf
+checkInstallation gcc-c++
+checkInstallation python-devel
+checkInstallation zlib-devel
+checkInstallation openssl-devel
+checkInstallation libpcap-devel.x86_64
+checkInstallation subversion
+checkInstallation screen
+checkInstallation glibc-devel
+checkInstallation python2.5
+checkInstallation scapy
+checkInstallation pyrit
+checkInstallation cpyrit-cuda
+checkInstallation cowpatty
+checkInstallation crunch
